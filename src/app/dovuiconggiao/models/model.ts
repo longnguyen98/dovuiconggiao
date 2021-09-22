8e0f57d7d5979e4c96fb273b288c14a0a9608237
long.nguyen-hoang2 <long.nguyen-hoang2@banvien.com.vn>
2021-09-22 15:58:00 +0700

    data_model

---
+++ src/app/dovuiconggiao/models/model.ts
@@ -0,0 +1,46 @@
+export interface Question {
+  id: string;
+  content: string;
+  img: string;
+  options: Option[];
+  topics: Topic[]
+  author: User;
+  authorId: string
+}
+
+export interface Option {
+  id: string;
+  content: string;
+  img: string;
+  correct: boolean;
+  questionId: string;
+}
+
+export interface User{
+  id: string;
+  name0: string;
+  name1: string;
+  name2: string;
+  name3: string;
+  location: string;
+  avatar: string;
+  email: string;
+}
+
+export interface Topic {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export interface Record{
+  id: string;
+  topic: Topic;
+  topicId: string;
+  score: number;
+  time: string;
+  createdDate: string;
+  user: User;
+  userId: string;
+}
+
