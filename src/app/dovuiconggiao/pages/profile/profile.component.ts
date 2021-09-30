bfaba9a814ab5abc1abe9a16ccb3da9adc80d024
tuanngoc1999 <45565250+tuanngoc1999@users.noreply.github.com>
2021-09-30 09:05:24 +0700

    Update profile screen

--- src/app/dovuiconggiao/pages/profile/profile.component.ts
+++ src/app/dovuiconggiao/pages/profile/profile.component.ts
@@ -7,6 +7,8 @@ import {QuestionsService} from "../../services/questions.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SecurityUtil} from "../../utils/security.util";
 import Swal from "sweetalert2";
+import { UsersService } from '../../services/users.service';
+import {DocumentSnapshot} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-profile',
@@ -17,7 +19,7 @@ export class ProfileComponent implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['id', 'content', 'topics', 'actions'];
   ELEMENT_DATA: Question[] = [];
   dataSource = new MatTableDataSource<Question>(this.ELEMENT_DATA);
-  isEdit: boolean = false;
+  isEdit = false;
   userId: string | null | undefined;
   user: User;
 
@@ -26,18 +28,33 @@ export class ProfileComponent implements AfterViewInit, OnInit {
   constructor(private questionsService: QuestionsService
     , private activatedRoute: ActivatedRoute
     , private router: Router
-    , private security: SecurityUtil) {
+    , private security: SecurityUtil
+    , private userService: UsersService) {
     this.userId = activatedRoute.snapshot.paramMap.get('id');
     if (this.userId) {
-
     } else {
       security.getCurrentUser((user: User) => {
-        this.user = user;
+        //  this.user = user;
+        //  console.log(user);
+        this.userService.get(user.id, (qs:any) => {
+          this.user = <User>qs.data();
+          if(!this.user.avatar) {
+            this.user.avatar = user.avatar;
+          }
+          if(!this.user.name0) {
+          this.user.name0 = user.name0;
+          }
+        }, (err: any) => {
+          Swal.fire('Úi! có lỗi rồi! Chụp ảnh màn hình rồi gửi mấy bạn Dev nha', err, 'error').then(r => {
+            console.log(err);
+          });
+        });
+    
       }, () => {
       });
     }
   }
-
+  
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
@@ -51,7 +68,24 @@ export class ProfileComponent implements AfterViewInit, OnInit {
       this.ELEMENT_DATA.sort((a, b) => new Date(b.createdTime).getTime() - new Date(a.createdTime).getTime());
       this.dataSource.data = this.ELEMENT_DATA;
     });
-
+    // if (this.userId) {
+    //   console.log(this.user);
+    //   this.userService.get(this.userId, (ds: DocumentSnapshot<User>) => {
+    //     if (ds.exists) {
+    //       this.user.location = ds.data().location;
+    //       this.user.email = ds.data().email;
+    //       this.user.name3 = ds.data().name3;
+    //     } else {
+    //       Swal.fire('Úi! có lỗi rồi! Chụp ảnh màn hình rồi gửi mấy bạn Dev nha', '404', 'error').then(r => {
+    //         console.log('Document doesn\'t exist: ' + this.userId);
+    //       });
+    //     }
+    //   }, (err: any) => {
+    //     Swal.fire('Úi! có lỗi rồi! Chụp ảnh màn hình rồi gửi mấy bạn Dev nha', err, 'error').then(r => {
+    //       console.log(err);
+    //     });
+    //   });
+    // }
   }
 
   onEditProfile(): void {
@@ -60,5 +94,29 @@ export class ProfileComponent implements AfterViewInit, OnInit {
     } else {
       this.isEdit = true;
     }
+  }
+
+  onSaveProfile(): void {
+    this.showLoading();
+    console.log(this.user);
+    this.userService.update(this.user.id, this.user, () => {
+      Swal.close();
+      Swal.fire('OK gòi đó!', '', 'success').then(r => {
+        this.isEdit = false;
+      });
+    }, (err: any) => {
+      Swal.fire('Úi! có lỗi rồi! Chụp ảnh màn hình rồi gửi mấy bạn Dev nha', err, 'error').then(r => {
+        console.log(err);
+      });
+    });
+  }
+
+  showLoading(): void {
+    Swal.fire({
+      title: 'Chờ xíu nha!',
+      html: '<img src="/assets/loading.gif" alt="loading" style="width: 100px;height: auto"/>',
+      showCancelButton: false,
+      showConfirmButton: false
+    });
   }
 }
