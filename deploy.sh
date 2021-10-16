65950e33529190e3d880fee08717d406689ee8c9
long.nguyen-hoang2 <long.nguyen-hoang2@banvien.com.vn>
2021-10-16 19:01:35 +0700

    deploy_script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ng build
+rm -r dist/firebase_deploy/public/*
 cd dist/dovuiconggiao
-cp -R * ../firebase_deploy
+cp -R * ../firebase_deploy/public
 cd ../firebase_deploy
-firebase deploy -p ./
+firebase deploy -p ./public
 date +"%T"
